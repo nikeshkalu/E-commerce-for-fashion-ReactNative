beecded734233cc065d4934406372d0563ae6823
Nikesh Kalu <nikesh.kalu@deerwalk.edu.np>
2020-10-22 19:38:26 +0545

    project Initialized

---
+++ src/authentication/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable prettier/prettier */
+export { default as onBoarding } from "./onBoarding";
---
+++ src/authentication/onBoarding/onBoarding.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import React from "react";
+import { View } from "react-native";
+
+const onBoarding = () => {
+  return <View style={{ flex:1 , backgroundColor : "gray" }} />;
+};
+
+export default onBoarding;
