c2a43e0198b065917a1576a0e0b8718d7ad090ad
Nikesh Kalu <nikesh.kalu@deerwalk.edu.np>
2020-11-18 15:52:18 +0545

    Slider Started for project

--- src/authentication/onBoarding/onBoarding.tsx
+++ src/authentication/onBoarding/onBoarding.tsx
@@ -1,9 +1,55 @@
-/* eslint-disable prettier/prettier */
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet, Dimensions } from "react-native";
+// import { ScrollView } from "react-native-gesture-handler";
+// import {useValue,onScrollEvent} from "react-native-redash"
+import Animated from 'react-native-reanimated'
+import Slide,{SLIDER_HEIGHT} from './Slide'
+
+
+const { width} = Dimensions.get("window")
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor : "white",
+  },
+  slider: {
+    height: SLIDER_HEIGHT,
+    backgroundColor : "cyan",
+    borderBottomRightRadius : 75
+  },
+  footer: {
+    flex: 1,
+  },
+});
 
 const onBoarding = () => {
-  return <View style={{ flex:1 , backgroundColor : "gray" }} />;
+  // const x = useValues(0);
+  // const onScroll = onScrollEvent({ x })
+  return (
+    <View style={styles.container}>
+      <View style={styles.slider}>
+        <Animated.ScrollView 
+          horizontal 
+          snapToInterval={width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+          // {...{ onScroll }}
+          >
+          <Slide label="Relaxed" />
+          <Slide label="Painful" right/>
+          <Slide label="Excentric"/>
+          <Slide label="Funky" right/>
+        </Animated.ScrollView>
+      </View>
+      <View style={styles.footer}>
+        <View
+          style={{...StyleSheet.absoluteFillObject,backgroundColor: "cyan"}}/>
+          <View style={{flex:1,backgroundColor: "white",borderTopLeftRadius:75}}></View>
+        </View>
+    </View>
+  );
 };
 
 export default onBoarding;
